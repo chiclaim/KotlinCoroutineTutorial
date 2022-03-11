0222d10f469501c03581a1c7b7111339679c68d4
chiclaim <chiclaim@gmail.com>
2022-03-11 16:15:01 +0800

    first commit

---
+++ app/src/main/java/com/chiclaim/coroutine_tutorial/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.chiclaim.coroutine_tutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
